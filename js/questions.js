c15fda42020214a6bc90863d3b6818b741f617b6
unknown <dhruvsarkhandia0@gmail.com>
2022-08-05 09:04:29 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What ten letter word starts with gas?",
+    answer: "Automobile",
+    options: [
+      "Retirement",
+      "Automobile",
+      "Aberration",
+      "Television"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What comes once in a minute, twice in a moment, but never in a thousand years?",
+    answer: "The letter 'M",
+    options: [
+      "Thirty-one seconds",
+      "1/1000 of a decade",
+      "One-tenth of a century",
+      "The letter 'M"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How active is your imagination?",
+    answer: "Very active; I am always daydreaming.",
+    options: [
+      "Not active at all; I'm too logical.",
+      "Very active; I am always daydreaming.",
+      "Somewhat active; I invent things in my head from time to time.",
+      "I'm actually not sure."
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is an assumption?",
+    answer: "A perspective you may have.",
+    options: [
+      "An idea you take for granted.",
+      "A perspective you may have.",
+      "A piece of information you have researched.",
+      "The answer to a question."
+
+    ]
+  },
+    {
+    numb: 5,
+    question: "Intelligent mistakes...?",
+    answer: "Usually can't be avoided.",
+    options: [
+      "Could often be prevented by planning ahead.",
+      "Don't serve a purpose.",
+      "Are never beneficial.",
+      "Usually can't be avoided."
+    ]
+  },
+
+  {
+  numb: 6,
+  question: "When I get multiplied by any number, the sum of the figures in the product is always me. What am I?",
+  answer: "9",
+  options: [
+    "9",
+    "8",
+    "2",
+    "4"
+  ]
+}
+];
